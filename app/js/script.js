b611a07086a9bcb4ce40c9094d438b42fb544532
James Lusk <jamesallenlusk@gmail.com>
2021-01-27 19:20:23 -0700

    added hero section

--- app/js/script.js
+++ app/js/script.js
@@ -1,17 +1,27 @@
 const btnMenu = document.querySelector('#btnMenu');
+const body = document.querySelector('body');
 const header = document.querySelector('.header');
 const overlay = document.querySelector('.overlay');
+const fadeElems = document.querySelectorAll('.has-fade');
 
 btnMenu.addEventListener('click', function () {
     console.log("open menu");
 
     if (header.classList.contains('open')) { // Close the Menu
+        body.classList.remove('lockScroll');
         header.classList.remove('open');
-        overlay.classList.remove('fade-in');
-        overlay.classList.add('fade-out');
+        fadeElems.forEach(function (element) {
+            element.classList.remove('fade-in');
+            element.classList.add('fade-out');
+        });
+
     } else { // Open Menu
+        body.classList.add('lockScroll');
         header.classList.add('open');
-        overlay.classList.remove('fade-out');
-        overlay.classList.add('fade-in');
+        fadeElems.forEach(function (element) {
+            element.classList.remove('fade-out');
+            element.classList.add('fade-in');
+        });
+
     }
 });
